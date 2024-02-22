e9dcc3ffb84e7dea1ed032d202357afe4e627c5c
Midly <svangasteren05@gmail.com>
2024-02-22 17:41:23 +0100

    Add project files.

---
+++ ZenithDiscordBot/Program.cs
@@ -0,0 +1,55 @@
+﻿using DSharpPlus;
+using DSharpPlus.CommandsNext;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ZenithDiscordBot.commands;
+
+namespace ZenithBot
+{
+    internal class Program
+    {
+        private static DiscordClient Client { get; set; }
+        private static CommandsNextExtension Commands { get; set; }
+        static async Task Main(string[] args)
+        {
+            var jsonReader = new ZenithDiscordBot.config.JSONReader();
+            await jsonReader.ReadJSON();
+
+            var discordConfig = new DiscordConfiguration()
+            {
+                Intents = DiscordIntents.All, 
+                Token = jsonReader.token,
+                TokenType = TokenType.Bot,
+                AutoReconnect = true
+            };
+
+            Client = new DiscordClient(discordConfig);
+
+            Client.Ready += Client_Ready;
+
+            var commandsConfig = new CommandsNextConfiguration()
+            {
+                StringPrefixes = new string[] { jsonReader.prefix },
+                EnableMentionPrefix = true,
+                EnableDms = true,
+                EnableDefaultHelp = true, //default help, turn off later
+            };
+
+            Commands = Client.UseCommandsNext(commandsConfig);
+
+            Commands.RegisterCommands<ZenithCommands>();
+
+            await Client.ConnectAsync();
+            await Task.Delay(-1);
+        }
+
+        private static Task Client_Ready(DiscordClient sender, DSharpPlus.EventArgs.ReadyEventArgs args)
+        {
+            return Task.CompletedTask;
+        }
+    }
+}
---
+++ ZenithDiscordBot/commands/ZenithCommands.cs
@@ -0,0 +1,29 @@
+﻿using DSharpPlus.CommandsNext;
+using DSharpPlus.CommandsNext.Attributes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ZenithDiscordBot.commands
+{
+    public class ZenithCommands : BaseCommandModule
+    {
+        [Command("whoami")]
+        public async Task whoamiCommand(CommandContext ctx)
+        {
+            await ctx.Channel.SendMessageAsync($"Account ping: {ctx.User.Mention}");
+            await ctx.Channel.SendMessageAsync($"Account username: {ctx.User.Username}");
+            await ctx.Channel.SendMessageAsync($"Account ID: {ctx.User.Id}");
+            await ctx.Channel.SendMessageAsync($"Account creation date: {ctx.User.CreationTimestamp}");
+            await ctx.Channel.SendMessageAsync($"Account PFP: {ctx.User.AvatarUrl}");
+        }
+
+        [Command("logo")]
+        public async Task logoCommand(CommandContext ctx)
+        {
+            await ctx.Channel.SendMessageAsync($"High resolution discord logo: {ctx.User.DefaultAvatarUrl}!");
+        }
+    }
+}
